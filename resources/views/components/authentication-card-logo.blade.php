f4825af08b75884cb9db7afbd6a85313d1d1c5b9
draletilouis <dralouis01@gmail.com>
2026-05-23 13:50:01 +0300

    Add missing authentication card logo component

---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,8 @@
+<a href="{{ route('dashboard') }}" class="inline-flex items-center justify-center">
+    <img
+        src="{{ asset('images/logo.png') }}"
+        alt="{{ config('app.name', 'Kashtre') }} Logo"
+        class="h-12 w-auto object-contain"
+        style="max-width: 180px;"
+    />
+</a>
