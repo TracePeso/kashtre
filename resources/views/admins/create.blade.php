d90c91040934c6500e775a9d784f7ebe8489880a
katznicho <katznicho@gmail.com>
2026-06-03 22:47:15 +0300

    end-no-edit

--- resources/views/admins/create.blade.php
+++ resources/views/admins/create.blade.php
@@ -11,6 +11,7 @@ $servicePoints = ServicePoint::where('business_id', $businessId)->get();
 @endphp
 
 
+
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
